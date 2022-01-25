6acb36545f9fc2ee1e28183bdadb65b30b610e88
PATILAVINASHG <91301965+PATILAVINASHG@users.noreply.github.com>
2022-01-25 10:48:39 +0530

    Delete UserName.py

--- UserName.py
+++
@@ -1,7 +0,0 @@
-text = "Hello << UserName>>,how are you"
-username = input("Enter the user name : ")
-if len(username )>=3:
- newtext = text.replace("UserName", username)
-else:print("enter the min 3 character")
-print(newtext)
-
